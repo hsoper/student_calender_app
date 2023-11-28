d45e9d575ff992b69da9b3967972d7cdf0015e2c
Humberto Soperanez Moya <hsoperanez@outlook.com>
2023-11-27 23:32:27 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/studnet_calender_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.studnet_calender_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Run the app and pass in the SettingsController. The app listens to the
+  // SettingsController for changes, then passes it further down to the
+  // SettingsView.
+  runApp(const MaterialApp());
+}
